9cc1fa53f8e7aab41faa6f044a1ec020f621e695
power <power228@qq.com>
2019-03-24 23:07:14 +0800

    Signed-off-by: power <power228@qq.com>

--- lemon_free_python/class_1106_object/run.py
+++ lemon_free_python/class_1106_object/run.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2018/11/16 16:00
-# @Author  : lemon_huahua
-# @Email   : 204893985@qq.com
 # @File    : run.py
 
 import pandas as pd
-from class_1106_object.http_request import HttpRequest
+from lemon_free_python.class_1106_object.http_request import HttpRequest
 
 #打开Excel获取所有的测试数据
 df=pd.read_excel('test_data.xlsx')
 test_data=df.values#嵌套列表的数据
+print(test_data)
 
-#完成HTTP测试
-for item in test_data:
-    print("目前正在执行的是用例{0}：{1}".format(item[0],item[1]))
-    res=HttpRequest.http_request(item[2],eval(item[3]),item[4])
-    print('http执行的结果是{0}'.format(res))
+# #完成HTTP测试
+# for item in test_data:
+#     print("目前正在执行的是用例{0}：{1}".format(item[0],item[1]))
+#     res=HttpRequest.http_request(item[2],eval(item[3]),item[4])
+#     print('http执行的结果是{0}'.format(res))
+
+login_url = 'http://47.107.168.87:8080/futureloan/mvc/api/member/login'
+data = {"mobilephone":"18688773467","pwd":"123456"}
+res=HttpRequest.http_request(login_url,data,'GET')
+print(res)
 
 #加入单元测试
 #框架
